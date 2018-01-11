b67f8044e6bb4ef4562edadd3998e916856f2664
mitroskii <mitro.onatsu@hotmail.com>
2018-01-11 11:03:09 +0200

    TaskPerson Example done

---
+++ object-method/TaskPerson/Person.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskPerson
+{
+    class Person
+    {
+        //Fields
+        public string Name;
+        private int Age;
+
+        //Constructor
+        public Person()
+        {
+            Name = "unknow";
+            Age = 0;
+        }
+            
+            public Person(string name, int age)
+        {
+            Name = name;
+            this.Age = age;
+        }
+
+        //Methods
+        public void GrowOld()
+        {
+            this.Age++;
+        }
+
+        public void PrintPersonInfo()
+        {
+            Console.WriteLine($"Nimi: {Name}\nIkä: {this.Age}\nAikuinen: {isAdult()}");
+        }
+
+        public bool isAdult()
+        {
+            //return this.age >= 18;
+
+            if (this.Age >= 18)
+                return true;
+            return false;
+        }
+        
+        public int GetAge()
+        {
+            return this.Age;
+        }
+    }
+}
---
+++ object-method/TaskPerson/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TaskPerson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person personJouni = new Person();
+            personJouni.Name = "Jouni K";
+            personJouni.GrowOld();
+            personJouni.PrintPersonInfo();
+
+            Person newPerson = new Person("Joe Doe", 17);
+            newPerson.PrintPersonInfo();
+            newPerson.GrowOld();
+            newPerson.PrintPersonInfo();
+
+            Console.ReadKey();
+        }
+    }
+}
