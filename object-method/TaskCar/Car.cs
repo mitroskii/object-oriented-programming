c73e27d024ebceff591c5fd5523bd52aba5f8929
mitroskii <mitro.onatsu@hotmail.com>
2018-01-12 11:02:08 +0200

    TaskCar started

---
+++ object-method/TaskCar/Car.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskCar
+{
+    class Car
+    {
+        //Fields
+        public string Brand;
+        public int Speed;
+        private int _brake;
+
+        //Constructor
+        public Car()
+        {
+            Brand = "unknow";
+            Speed = 0;
+            _brake = Speed;
+        }
+
+        public Car(string brand, int speed)
+        {
+            Brand = brand;
+            this.Speed = speed;
+        }
+
+        //Methods
+        public void AskData()
+        {
+            Console.WriteLine($"Syötä auton merkki: ");
+            string Brand = Console.ReadLine();
+            Console.WriteLine($"Syötä auton nopeus: ");
+            int Speed = int.Parse(Console.ReadLine());
+        }
+
+        public void ShowCarInfo()
+        {
+            Console.WriteLine($"Auton merkki: {Brand}\n Auton nopeus: {Speed}");
+        }
+
+        public void Accelerate()
+        {
+
+        }
+
+        private void Brake()
+        {
+
+        }
+    }
+}
---
+++ object-method/TaskCar/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TaskCar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car newcar = new Car("Toyota", 80);
+            Car Car2 = new Car("Honda", 90);
+            Console.ReadKey();
+        }
+    }
+}
