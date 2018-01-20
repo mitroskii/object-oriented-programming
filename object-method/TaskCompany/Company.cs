62ec2945fe05db3605763b2678eecec77c270336
mitroskii <mitro.onatsu@hotmail.com>
2018-01-20 19:33:02 +0200

    TaskCompany done

---
+++ object-method/TaskCompany/Company.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskCompany
+{
+    class Company
+    {
+        //Fields
+        public string Title;
+        public string Address;
+        public int Phone;
+        public int Outcome;
+        public int Expense;
+
+        //Constructor
+        public Company()
+        {
+            Title = "Unknown";
+            Address = "Unknown";
+            Phone = 0;
+            Outcome = 0;
+            Expense = 0;
+        }
+
+        public Company(string title, string address, int phone, int outcome, int expense)
+        {
+            Title = title;
+            Address = address;
+            this.Phone = phone;
+            this.Outcome = outcome;
+            this.Expense = expense;
+        }
+
+        //Copy Constructor
+        public Company(Company previousCompany)
+        {
+            Title = previousCompany.Title;
+            Address = previousCompany.Address;
+            this.Phone = previousCompany.Phone;
+            this.Outcome = previousCompany.Outcome;
+            this.Expense = previousCompany.Expense;
+        }
+
+        //Methods
+        public void AskData()
+        {
+            Console.WriteLine("Yrityksen nimi: ");
+            Title = Console.ReadLine();
+            Console.WriteLine("Osoite: ");
+            Address = Console.ReadLine();
+            Console.WriteLine("Puhelinnumero: ");
+            Phone = int.Parse(Console.ReadLine());
+            Console.WriteLine("Tulot: ");
+            Outcome = int.Parse(Console.ReadLine());
+            Console.WriteLine("Menot: ");
+            Expense = int.Parse(Console.ReadLine());
+        }
+
+        public void ShowCompanyInfo()
+        {
+            Console.WriteLine($"Nimi: {Title}\n" +
+                              $"Osoite: {Address}\n" +
+                              $"Puhelinnumero: {Phone}\n" +
+                              $"Tulot: {Outcome}\n" +
+                              $"Menot: {Expense}");
+        }
+
+        public void Profit(Company PreviousCompany)
+        {
+            int Profit = (PreviousCompany.Outcome - PreviousCompany.Expense) / PreviousCompany.Expense * 100;
+
+            if (Profit < 100)
+            {
+                Console.WriteLine($"Yrityksellä menee kehnosti.");
+            }
+            else if(Profit >= 100 && Profit <= 300)
+            {
+                Console.WriteLine($"Yrityksellä menee tyydyttävästi.");
+            }
+            else
+            {
+                Console.WriteLine($"Yrityksellä menee loistavasti.");
+            }
+        }
+    }
+}
---
+++ object-method/TaskCompany/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace TaskCompany
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Company company = new Company();
+            company.AskData();
+            Console.WriteLine();
+            company.ShowCompanyInfo();
+            company.Profit(company);
+            Console.WriteLine();
+
+            Company company2 = new Company();
+            company2.AskData();
+            Console.WriteLine();
+            company2.ShowCompanyInfo();
+            company2.Profit(company2);
+            Console.WriteLine();
+
+            Company company3 = new Company();
+            company3.AskData();
+            Console.WriteLine();
+            company3.ShowCompanyInfo();
+            company3.Profit(company3);
+            Console.WriteLine();
+
+            Console.ReadKey();
+        }
+    }
+}
