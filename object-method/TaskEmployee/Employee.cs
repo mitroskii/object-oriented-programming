56bfb0ca4ba9fc219fcda55eb891395178d3917f
mitroskii <mitro.onatsu@hotmail.com>
2018-01-21 22:23:10 +0200

    TaskEmployee started

---
+++ object-method/TaskEmployee/Employee.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskEmployee
+{
+    class Employee
+    {
+        //Fields
+        public string Name;
+        public int Id;
+        public string Position;
+        public int Salary;
+        public int NewSalary = 0;
+
+        //Constructor
+        public Employee()
+        {
+            Name = "Unknown";
+            Id = 0;
+            Position = "Unknow";
+            Salary = 0;
+        }
+
+        public Employee(string name, int id, string position, int salary)
+        {
+            Name = name;
+            this.Id = id;
+            Position = position;
+            this.Salary = salary;
+        }
+
+        //Methods
+       
+        public void PrintEmployeeInfo(Employee employee)
+        {
+            if (employee.Id == 12345)
+            {
+                NewSalary = employee.Salary;
+            }
+
+            Console.WriteLine($"Nimi: {employee.Name}\n" +
+                             $"Id: {employee.Id}\n" +
+                             $"Tehtävä: {employee.Position}\n" +
+                             $"Palkka: {employee.Salary}");           
+        }
+        public void CompareSalary(Employee employee)
+        {
+            int Comparison = 0;
+            Comparison = employee.Salary - Salary;
+        }
+
+    }
+}
---
+++ object-method/TaskEmployee/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TaskEmployee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee[] employees = new Employee[3];
+            employees[0] = new Employee("Tupu", 12345, "Boss", 15000 );
+            employees[0].PrintEmployeeInfo(employees[0]);
+            Console.WriteLine();
+            employees[1] = new Employee("Hupu", 23456, "Chief", 10000);
+            employees[1].PrintEmployeeInfo(employees[1]);
+            Console.WriteLine();
+            employees[2] = new Employee("Lupu", 34567, "Master", 5000);
+            employees[2].PrintEmployeeInfo(employees[2]);
+            Console.ReadKey();
+        }
+    }
+}
