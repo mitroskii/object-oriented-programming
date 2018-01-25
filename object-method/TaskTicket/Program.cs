01321053659aae62a5fc8e61b78ce8123f414cb8
mitroskii <mitro.onatsu@hotmail.com>
2018-01-25 10:01:25 +0200

    TaskTicket done

---
+++ object-method/TaskTicket/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TaskTicket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Only one discount is given. Except if you are Mtk member and studier, you'll get both discounts");
+            Ticket ticket = new Ticket();
+            ticket.AskData();
+            ticket.ShowTicketPrice(ticket);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ object-method/TaskTicket/Ticket.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskTicket
+{
+    class Ticket
+    {
+        //Fields
+        public int Age;
+        public int Price;
+        public float Discount;
+
+        //Constructor
+        public Ticket()
+        {
+            Age = 0;
+            Price = 16;
+            Discount = 0.0f;
+        }
+
+        public Ticket(int age, int price, int discount)
+        {
+            this.Age = age;
+            this.Price = price;
+            this.Discount = discount;
+        }
+
+        //Methods
+        public void AskData()
+        {
+            Console.Write("How old are you?: ");
+            Age = int.Parse(Console.ReadLine());
+        }
+
+        public void ShowTicketPrice(Ticket ticket)
+        {
+            if (Age < 7)
+                Console.WriteLine($"Ticket price = {Price * 0}€");
+            if (Age >= 7 && Age <= 15)
+                Console.Write($"Ticket price = {Price * 0.5}€");
+            if (Age >= 65)
+                Console.Write($"Ticket price = {Price * 0.5}€");
+
+            if (Age > 15 && Age < 65)
+            {
+                Console.Write("Are you soldier? [Yes/No]: ");
+                if (Console.ReadLine().ToUpper() == "YES")
+                 Console.WriteLine($"Ticket price = {Price * 0.5}€"); 
+
+                else
+                {
+                    Console.Write("Are you Mtk member? [Yes/No]: ");
+                    if (Console.ReadLine().ToUpper() == "YES")
+                    {
+                        Discount = 0.15f;
+                    }
+
+                    Console.Write("Are you studier? [Yes/No]: ");
+                    if (Console.ReadLine().ToUpper() == "YES")
+                    {
+                        Discount = Discount + 0.45f;
+                    }
+                    Console.WriteLine($"Ticket price = {Price - Price * Discount} €");
+                }
+            }
+        }
+    }
+}
+
